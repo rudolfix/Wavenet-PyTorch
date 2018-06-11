95eb4f6001f7b2a9c7b9a50bc8b905d9f750f58e
MasonMcGough <mcgough.mason@gmail.com>
2018-06-11 14:02:07 -0400

    Improve logic of normalize method on MuEncoder; Add docstrings to some MuEncoder methods;

--- wavenet/muencoder.py
+++ wavenet/muencoder.py
@@ -5,13 +5,50 @@ class MuEncoder(object):
         self.mu = mu
         self.datarange = datarange
 
-    def normalize(self, x):
-        width = self.datarange[1] - self.datarange[0]
-        return ((np.float32(x) - self.datarange[0]) / width - 0.5) * 2
+    def normalize(self, x, span=None):
+        """
+        Scale x from range of span to range [-1, 1]
+
+        The span can be specified using a string argument or using a length-2 
+        list-like. If not specified, normalize attempts to use self.datarange as 
+        span. If self.datarange is also None, then the min and max of x are used.
+        """
+
+        def _span_datarange(datarange):
+            lower = datarange[0]
+            span = datarange[1] - lower
+            return (span, lower)
+
+        def _span_minmax(x):
+            lower = np.min(x)
+            span = np.max(x) - lower
+            return (span, lower)
+
+        if span is None:
+            if self.datarange is not None:
+                span, lower = _span_datarange(self.datarange)
+            else:
+                span, lower = _span_minmax(x)
+        elif isinstance(span, str):
+            if span == 'datarange':
+                span, lower = _span_datarange(self.datarange)
+            elif span == 'minmax':
+                span, lower = _span_minmax(x)
+            else:
+                span, lower = _span_minmax(x)
+        else:
+            lower = span[0]
+            span = span[1] - lower
+
+        return ((np.float32(x) - lower) / span - 0.5) * 2
 
     def expand(self, x):
-        width = self.datarange[1] - self.datarange[0]
-        return (x / 2 + 0.5) * width + self.datarange[0]
+        """
+        Scale x from range of [-1, 1] to self.datarange
+        """
+
+        span = self.datarange[1] - self.datarange[0]
+        return (x / 2 + 0.5) * span + self.datarange[0]
 
     def encode(self, x):
         x = self.normalize(x)
