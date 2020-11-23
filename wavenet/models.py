b28fa9d9020363cd6dd2b49b41425295a47afd7b
Marcin Rudolf <marcin.rudolf@neufund.org>
2020-11-23 13:40:20 +0100

    replaces visdom with tensorboard, fixes sample audio generation

--- wavenet/models.py
+++ wavenet/models.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import visdom
+from torch.utils.tensorboard import SummaryWriter
 
 
 class Model(nn.Module):
@@ -83,7 +83,7 @@ class Model(nn.Module):
             self.device = device
 
     def train(self, dataloader, num_epochs=25, validation=False, disp_interval=None, use_visdom=False):
-        since = time.time()
+        # since = time.time()
         
         self.to(self.device)
 
@@ -93,10 +93,13 @@ class Model(nn.Module):
             phase = 'Training'
 
         if use_visdom:
-            vis = visdom.Visdom()
+            writer = SummaryWriter()
+            # writer.add_graph(self)
+            # writer.flush()
             gen = Generator(self, dataloader.dataset)
         else:
-            vis = None
+            writer = None
+            gen = None
 
         losses = []
         for epoch in range(1, num_epochs + 1):
@@ -107,7 +110,7 @@ class Model(nn.Module):
                 
             # reset loss for current phase and epoch
             running_loss = 0.0
-            running_corrects = 0
+            # running_corrects = 0
             
             for inputs, labels in dataloader:
                 inputs = inputs.to(self.device)
@@ -123,9 +126,11 @@ class Model(nn.Module):
                     if not validation:
                         loss.backward()
                         self.optimizer.step()
-                        self.scheduler.step()
                         
                 running_loss += loss.item() * inputs.size(1)
+
+            if not validation:
+                self.scheduler.step()
 
             losses.append(running_loss)
             if disp_interval is not None and epoch % disp_interval == 0:
@@ -137,73 +142,104 @@ class Model(nn.Module):
                 print()
                 with NamedTemporaryFile("wb", prefix="wavenet_", delete=False) as f:
                     print('Saving model data to file: {}'.format(f.name))
-                    torch.save(self.state_dict(), f)
+                    self.save_state(f)
 
-                if vis is not None:
+                if writer is not None:
                     # display network weights
-                    for m in self.hs:
-                        _vis_hist(vis, m.gatedconv.conv_f.weight, 
-                                  m.name + ' ' + 'W-conv_f')
-                        _vis_hist(vis, m.gatedconv.conv_g.weight, 
-                                  m.name + ' ' + 'W-conv_g')
-                        _vis_hist(vis, m.gatedconv.conv_f.bias,
-                                  m.name + ' ' + 'b-conv_f')
-                        _vis_hist(vis, m.gatedconv.conv_g.bias, 
-                                  m.name + ' ' + 'b-conv_g')
+                    # for m in self.hs:
+                    #     _vis_hist(vis, m.gatedconv.conv_f.weight,
+                    #               m.name + ' ' + 'W-conv_f')
+                    #     _vis_hist(vis, m.gatedconv.conv_g.weight,
+                    #               m.name + ' ' + 'W-conv_g')
+                    #     _vis_hist(vis, m.gatedconv.conv_f.bias,
+                    #               m.name + ' ' + 'b-conv_f')
+                    #     _vis_hist(vis, m.gatedconv.conv_g.bias,
+                    #               m.name + ' ' + 'b-conv_g')
 
                     # display raw outputs
-                    _vis_hist(vis, outputs, 'Outputs')
+                    # _vis_hist(vis, outputs, 'Outputs')
 
                     # display loss over time
-                    _vis_plot(vis, np.array(losses) / len(dataloader), 'Losses')
+                    writer.add_scalar("Loss", epoch_loss, epoch)
+
+                    # learning rate
+                    writer.add_scalar("Learning Rate", self.scheduler.get_last_lr()[0], epoch)
 
                     # display audio sample
+                    i = gen.tensor2numpy(inputs[0].cpu()).reshape([-1])
+                    t = gen.dataset.encoder.unapply_mu(i)
+                    y = gen.run(inputs[0], dataloader.dataset.sample_rate, disp_interval=10, raw_data=True).reshape([-1])
+                    writer.add_audio("Input Sample", t, epoch, dataloader.dataset.sample_rate)
+                    writer.add_audio("Input Sample Raw", i, epoch, dataloader.dataset.sample_rate)
+                    writer.add_audio("Prediction", gen.dataset.encoder.unapply_mu(y), epoch, dataloader.dataset.sample_rate)
+                    writer.add_audio("Prediction Raw", y, epoch, dataloader.dataset.sample_rate)
                     # _vis_audio(vis, gen, inputs[0], 'Sample Audio', n_samples=44100,
                     #            sample_rate=dataloader.dataset.sample_rate)
 
+                    writer.flush()
 
-def _flatten(t):
-    t = t.to(torch.device('cpu'))
-    return t.data.numpy().reshape([-1])
+    def save_state(self, f):
+        state = {
+            "model": self.state_dict(),
+            "optimizer": self.optimizer.state_dict(),
+            "scheduler": self.scheduler.state_dict()
+        }
+        torch.save(state, f)
 
-def _vis_hist(vis, t, title):
-    vis.histogram(_flatten(t), win=title, opts={'title': title})
+    def load_state(self, f):
+        state = torch.load(f)
+        self.optimizer.load_state_dict(state["optimizer"])
+        self.scheduler.load_state_dict(state["scheduler"])
+        self.load_state_dict(state["model"])
 
-def _vis_plot(vis, t, title):
-    vis.line(t, X=np.array(range(len(t))), win=title, opts={'title': title})
 
-def _vis_audio(vis, gen, t, title, n_samples=50, sample_rate=44100):
-    y = gen.run(t, n_samples, disp_interval=10).reshape([-1])
-    t = gen.dataset.encoder.decode(gen.tensor2numpy(t.cpu())).reshape([-1])
-    audio = np.concatenate((t, y))
-    vis.audio(audio, win=title, 
-              opts={'title': title, 'sample_frequency': sample_rate})
+# def _flatten(t):
+#     t = t.to(torch.device('cpu'))
+#     return t.data.numpy().reshape([-1])
+#
+#
+# def _vis_hist(vis, t, title):
+#     vis.histogram(_flatten(t), win=title, opts={'title': title})
+#
+#
+# def _vis_plot(vis, t, title):
+#     vis.line(t, X=np.array(range(len(t))), win=title, opts={'title': title})
+#
+#
+# def _vis_audio(vis, gen, t, title, n_samples=50, sample_rate=44100):
+#     y = gen.run(t, n_samples, disp_interval=10).reshape([-1])
+#     t = gen.dataset.encoder.decode(gen.tensor2numpy(t.cpu())).reshape([-1])
+#     audio = np.concatenate((t, y))
+#     vis.audio(audio, win=title,
+#               opts={'title': title, 'sample_frequency': sample_rate})
+
 
 class GatedConv1d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, 
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0,
                  dilation=1, groups=1, bias=True):
         super(GatedConv1d, self).__init__()
         self.dilation = dilation
         self.conv_f = nn.Conv1d(in_channels, out_channels, kernel_size, 
-                                stride=stride, padding=padding, dilation=dilation, 
+                                stride=stride, padding=padding, dilation=dilation,
                                 groups=groups, bias=bias)
         self.conv_g = nn.Conv1d(in_channels, out_channels, kernel_size, 
-                                stride=stride, padding=padding, dilation=dilation, 
+                                stride=stride, padding=padding, dilation=dilation,
                                 groups=groups, bias=bias)
         self.sig = nn.Sigmoid()
 
     def forward(self, x):
-        padding = self.dilation - (x.shape[-1] + self.dilation - 1) % self.dilation
+        # padding = self.dilation - (x.shape[-1] + self.dilation - 1) % self.dilation
         x = nn.functional.pad(x, (self.dilation, 0))
         return torch.mul(self.conv_f(x), self.sig(self.conv_g(x)))
 
+
 class GatedResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, output_width, stride=1, padding=0, 
+    def __init__(self, in_channels, out_channels, kernel_size, output_width, stride=1, padding=0,
                  dilation=1, groups=1, bias=True):
         super(GatedResidualBlock, self).__init__()
         self.output_width = output_width
         self.gatedconv = GatedConv1d(in_channels, out_channels, kernel_size, 
-                                     stride=stride, padding=padding, 
+                                     stride=stride, padding=padding,
                                      dilation=dilation, groups=groups, bias=bias)
         self.conv_1 = nn.Conv1d(out_channels, out_channels, 1, stride=1, padding=0,
                                 dilation=1, groups=1, bias=bias)
@@ -215,6 +251,7 @@ class GatedResidualBlock(nn.Module):
         skip_cut = skip.shape[-1] - self.output_width
         skip = skip.narrow(-1, skip_cut, self.output_width)
         return residual, skip
+
 
 class Generator(object):
     def __init__(self, model, dataset):
@@ -236,8 +273,10 @@ class Generator(object):
         self.model.to(self.model.device)
         return self.model(x)
 
-    def run(self, x, num_samples, disp_interval=None):
-        x = self.dataset._to_tensor(self.dataset.preprocess(x))
+    def run(self, x, num_samples, disp_interval=None, raw_data=False):
+        if not raw_data:
+            x = self.dataset._to_tensor(self.dataset.preprocess(x))
+
         x = torch.unsqueeze(x, 0)
 
         y_len = self.dataset.y_len
@@ -249,7 +288,7 @@ class Generator(object):
 
             y_i = self.tensor2numpy(self.predict(x).cpu())
             y_i = self.dataset.label2value(y_i.argmax(axis=1))[0]
-            y_decoded = self.dataset.encoder.decode(y_i)
+            y_decoded = y_i if raw_data else self.dataset.encoder.decode(y_i)
 
             out[n_predicted:n_predicted + len(y_decoded)] = y_decoded
             n_predicted += len(y_decoded)
